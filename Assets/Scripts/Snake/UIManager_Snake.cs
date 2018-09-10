c28be77cbe5d327c0a9a9b11080b76af4681b4eb
Nagunt <reiy0902@naver.com>
2018-09-11 03:22:05 +0900

    UI Test Enabled

---
+++ Assets/Scripts/Snake/UIManager_Snake.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager_Snake : MonoBehaviour {
+
+    static UIManager_Snake uiManager = null;
+    static Vector2 screenSize = Vector2.zero;
+
+    static bool isInitalized = false;
+    public static bool IsInitalized
+    {
+        get
+        {
+            if (!isInitalized)
+            {
+                Init();
+                return false;
+            }
+            return isInitalized;
+        }
+    }
+
+    public static void Init()
+    {
+        if (!isInitalized)
+        {
+            if(uiManager == null)
+            {
+                uiManager = GameObject.Find("Canvas").GetComponent<UIManager_Snake>();
+                uiManager.InitUI();
+                isInitalized = true;
+            }
+        }
+    }
+
+    void Start()
+    {
+        Init();
+    }
+
+    void InitUI()
+    {
+        screenSize = new Vector2(Screen.width, Screen.height);
+        RectTransform upImageTransform = transform.Find("UpImage").GetComponent<RectTransform>();
+        RectTransform downImageTransform = transform.Find("DownImage").GetComponent<RectTransform>();
+        RectTransform leftImageTransform = transform.Find("LeftImage").GetComponent<RectTransform>();
+        RectTransform rightImageTransform = transform.Find("RightImage").GetComponent<RectTransform>();
+        Vector2 updownSizeVector = new Vector2(screenSize.x, screenSize.y / 18);
+        upImageTransform.sizeDelta = updownSizeVector;
+        downImageTransform.sizeDelta = updownSizeVector;
+        float gameScreenSizeValue = screenSize.y / 18 * 17;
+        leftImageTransform.sizeDelta = new Vector2(updownSizeVector.y, screenSize.y);
+        rightImageTransform.sizeDelta = new Vector2(screenSize.x - gameScreenSizeValue, screenSize.y);
+    }
+
+}
