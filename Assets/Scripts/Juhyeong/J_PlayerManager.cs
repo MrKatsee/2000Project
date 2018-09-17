dae64ef9eca28fad9c374df6883a3bedcd64a984
Iamgroooooot <dury.ko@gmail.com>
2018-09-18 01:26:52 +0900

    Basic AI 구현

---
+++ Assets/Scripts/Juhyeong/J_PlayerManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class J_PlayerManager : MonoBehaviour {
+
+    #region Singleton
+    public static J_PlayerManager instance;
+    public GameObject player;
+
+    void Awake()
+    {
+        instance = this;
+    }
+    #endregion
+}
