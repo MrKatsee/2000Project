a2b35487e2881972f569ee5ffd8d2eeabc6270a1
JUHYEONG-PC\duryk <dury.ko@gmail.com>
2018-09-17 22:31:47 +0900

    CIEN EnemyPoint 생성 완료

---
+++ Assets/Scripts/Juhyeong/J_EnemyAI.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class J_EnemyAI : MonoBehaviour {
+    float timer = 0;
+    public Vector2 HomePos;
+    float OriginX;
+    float OriginY;
+    float w;
+    float h;
+
+	// Use this for initialization
+	void Start ()
+    {
+        OriginX = transform.position.x;
+        OriginY = transform.position.y;
+        w = 1;
+        h = 1;
+        timer = 0;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        timer += 3f*Time.deltaTime;
+        MoveCircle();
+    }
+
+    private void MoveCircle()
+    {
+        
+
+
+        transform.position = new Vector2(OriginX + w*Mathf.Cos(timer),OriginY+h*Mathf.Sin(timer));
+
+
+    }
+}
