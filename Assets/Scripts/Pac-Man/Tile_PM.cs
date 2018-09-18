b8c574b54b4cec91a22c4d4e487412f1fb18b3dd
KimHoseong <ghtjd000129@naver.com>
2018-09-18 19:20:41 +0900

    팩맨 만들다맘

---
+++ Assets/Scripts/Pac-Man/Tile_PM.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile_PM : MonoBehaviour {
+
+    public int pNum_Tile = 0;
+    public Sprite[] tileSprite = new Sprite[3];
+
+    private void OnTriggerEnter2D(Collider2D c)
+    {
+        if (c.tag == "PacMan")
+        {
+            pNum_Tile = c.GetComponent<Character_PM>().pNum;
+            GetComponent<SpriteRenderer>().sprite = tileSprite[pNum_Tile];
+        }
+        
+    }
+}
